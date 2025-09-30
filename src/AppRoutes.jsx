535ae983770af282f1b7700aa07951fefeeee7ee
Luam-Novais <luampabl014@gmail.com>
2025-09-29 22:15:40 -0300

    refactor: reorganizar rotas e melhorar a estrutura do componente AppRoutes

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import PublicLayout from './layouts/PublicLayout/PublicLayout';
 import GlobalContext from './context/GlobalContext';
+import AuthLayout from './layouts/AuthLayout/AuthLayout';
+import Dashboard from './pages/Dashboard/Dashboard'
+import { useContext} from 'react';
+import { userContext } from './context/UserContext';
+
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <GlobalContext>
         <Routes>
-          <Route path="/" element={<PublicLayout />}>
-            <Route path="/login" element={<Login />} />
+          <Route path='/' element={<AuthLayout/>}/>
+          <Route element={<PublicLayout />}>
+            <Route path="login" element={<Login />} />
+          </Route>
+          <Route  element={<AuthLayout/>}>
+              <Route path='/dashboard' element={<Dashboard/>}/>
           </Route>
         </Routes>
       </GlobalContext>
