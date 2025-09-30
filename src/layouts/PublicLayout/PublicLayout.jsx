285b35dd8416d73bc17ef82cb8d67d1dd21d8555
Luam-Novais <luampabl014@gmail.com>
2025-09-29 22:12:35 -0300

    feat: adicionar contexto de usuário ao layout público

--- src/layouts/PublicLayout/PublicLayout.jsx
+++ src/layouts/PublicLayout/PublicLayout.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 import styles from './PublicLayout.module.scss'
+import { userContext } from '../../context/UserContext'
 
 const PublicLayout = ({children}) => {
   return (
