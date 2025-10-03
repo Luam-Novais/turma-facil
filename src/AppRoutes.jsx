1563e79c788f3d5e1c8ab08723052304d3947f96
Luam-Novais <luampabl014@gmail.com>
2025-10-02 21:36:27 -0300

    feat: organizar rotas públicas e adicionar página de erro

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,12 +1,15 @@
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login/Login';
+import Login from './pages/publicPages/Login/Login';
+import CreateAccount from './pages/publicPages/CreateAccount/CreateAccount';
 import PublicLayout from './layouts/PublicLayout/PublicLayout';
 import GlobalContext from './context/GlobalContext';
 import AuthLayout from './layouts/AuthLayout/AuthLayout';
-import Dashboard from './pages/Dashboard/Dashboard'
-import { useContext} from 'react';
-import { userContext } from './context/UserContext';
+import Dashboard from './pages/authPages/Dashboard/Dashboard'
+import Error from './pages/publicPages/Error/Error';
+import NotFound from './pages/publicPages/NotFound/NotFound'
+
+
 
 
 const AppRoutes = () => {
@@ -17,10 +20,13 @@ const AppRoutes = () => {
           <Route path='/' element={<AuthLayout/>}/>
           <Route element={<PublicLayout />}>
             <Route path="login" element={<Login />} />
+            <Route path="criar-conta" element={<CreateAccount />} />
           </Route>
           <Route  element={<AuthLayout/>}>
               <Route path='/dashboard' element={<Dashboard/>}/>
           </Route>
+          <Route path='/error-servidor' element={<Error/>}/>
+          <Route path='*' element={<NotFound/>} />
         </Routes>
       </GlobalContext>
     </BrowserRouter>
