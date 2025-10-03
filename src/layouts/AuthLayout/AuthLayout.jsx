b65326fa0ca80c5c3fa91e5f16ab128bccb51feb
Luam-Novais <luampabl014@gmail.com>
2025-10-02 21:32:53 -0300

    feat: adicionar Navbar ao layout de autenticação

--- src/layouts/AuthLayout/AuthLayout.jsx
+++ src/layouts/AuthLayout/AuthLayout.jsx
@@ -1,15 +1,18 @@
-import React, { useContext, useEffect } from 'react'
+import  { useContext} from 'react'
 import { Outlet, useNavigate } from 'react-router-dom'
 import styles from './AuthLayout.module.scss'
 import { userContext } from '../../context/UserContext'
-
+import Navbar from '../../components/Navbar/Navbar'
 
 const AuthLayout = () => {
     const {userAuthorized} = useContext(userContext)
     const navigate = useNavigate() 
     if(userAuthorized){
         return(
-            <main><Outlet/></main>
+            <main>
+               <Navbar/>
+                <Outlet/>
+            </main>
         )
     }
 }
