e1e4d632cfc0d60b8b0511c646390f990985a78f
Luam-Novais <luampabl014@gmail.com>
2025-10-06 13:39:26 -0300

    feat: implementar barra de navegação com ícones e animação de abertura

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import Loading from '../Loading/Loading';
+import styles from './Navbar.module.scss';
+import { IoIosHome } from 'react-icons/io';
+import { IoIosSearch } from 'react-icons/io';
+import { IoAddOutline } from 'react-icons/io5';
+import { IoIosList } from 'react-icons/io';
+import { IoIosArrowForward } from 'react-icons/io';
+import { IoIosArrowBack } from "react-icons/io";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  
+
+
   return (
-    <aside>
-      <nav>
-        <Loading/>
-        <NavLink>criar aluno</NavLink>
-        <NavLink>editar aluno</NavLink>
-        <NavLink>criar modalidade</NavLink>
-        <NavLink>listar alunos</NavLink>
-      </nav>
-    </aside>
+    <nav className={`${styles.sidebar} ${open ? styles.open : ''}`}>
+      <button onClick={() => setOpen(!open)}>
+        <i>
+          {!open ? <IoIosArrowForward/> : <IoIosArrowBack/>}
+        </i>
+      </button>
+      <ul className={`${open ? styles.listVisible : ''}`}>
+        <li className={styles.link}>
+          <NavLink to='/dashboard' className={({isActive}) => (isActive ? `${styles.active}` : '')}>
+            <i>
+              <IoIosHome />
+            </i>
+            <span className={`${open ? styles.visible : ''}`} >Inicio</span>
+          </NavLink>
+        </li>
+         <li className={styles.link}>
+          <NavLink to='/buscar' className={(isActive) => (isActive ? `${styles.isActive}` : '')}>
+            <i>
+              <IoIosSearch />
+            </i>
+            <span className={`${open ? styles.visible : ''}`}>Buscar</span>
+          </NavLink>
+        </li>
+         <li className={styles.link}>
+          <NavLink to='/add-new' className={(isActive) => (isActive ? `${styles.isActive}` : '')}>
+            <i>
+              <IoAddOutline />
+            </i>
+            <span className={`${open ? styles.visible : ''}`}>Adicionar</span>
+          </NavLink>
+        </li>
+        <li className={styles.link}>
+          <NavLink to='/listas' className={(isActive) => (isActive ? `${styles.isActive}` : '')}>
+            <i>
+              <IoIosList />
+            </i>
+            <span className={`${open ? styles.visible : ''}`}>Listar alunos</span>
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
