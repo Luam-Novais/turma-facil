e2b3a0f0e09f216904a36aa25ab8dedb7de6fd9d
Luam-Novais <luampabl014@gmail.com>
2025-10-07 21:10:07 -0300

    feat: add novas rotas

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -6,6 +6,9 @@ import PublicLayout from './layouts/PublicLayout/PublicLayout';
 import GlobalContext from './context/GlobalContext';
 import AuthLayout from './layouts/AuthLayout/AuthLayout';
 import Dashboard from './pages/authPages/Dashboard/Dashboard'
+import SearchModalidade from './pages/authPages/SearchModalidade/SearchModalidade';
+import ToListAlunos from './pages/authPages/ToListAlunos/ToListAlunos';
+import AddAlunosOrModalidade from './pages/authPages/AddAlunoOrModalidade/AddAlunosOrModalidade';
 import Error from './pages/publicPages/Error/Error';
 import NotFound from './pages/publicPages/NotFound/NotFound'
 
@@ -25,8 +28,8 @@ const AppRoutes = () => {
           <Route  element={<AuthLayout/>}>
               <Route path='/dashboard' element={<Dashboard/>}/>
               <Route path='/buscar-modalidade' element={<SearchModalidade/>}/>
-              <Route path='/add-aluno-modalidade' element={<AddALunoOrModalidade/>}/>
-              <Route path='/listar-alunos' element={<ToListALunos/>}/>
+              <Route path='/add-aluno-modalidade' element={<AddAlunosOrModalidade/>}/>
+              <Route path='/listar-alunos' element={<ToListAlunos/>}/>
           </Route>
           <Route path='/error-servidor' element={<Error/>}/>
           <Route path='*' element={<NotFound/>} />
