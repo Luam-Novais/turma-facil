0d600915cdbf65be3db51df36b2f5596752abe60
Luam-Novais <luampabl014@gmail.com>
2025-10-13 13:05:15 -0300

    feat: refatorar UserContext para usar hook useFetch e adicionar funções de criação de aluno e modalidade

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,28 +1,30 @@
 import React, { createContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import useFetch from '../hooks/useFetch';
+import formaterToken from '../utils/formaterToken';
 
 const API_URL = import.meta.env.VITE_API_URL
 
 export const userContext = createContext();
 const UserStorage = ({ children }) => {
+  const {request} = useFetch()
+  const token = formaterToken()
   const [userAuthorized, setUserAuthorized] = useState(false);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   async function userCreate(data) {
     setLoading(true);
     try {
-      const response = await fetch(`${API_URL}create-prof`, {
+      const responseCreate = await request(`${API_URL}create-prof`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(data),
       });
-      const json = await response.json();
-      console.log(response, json)
-      if (response.ok) {
+      if (responseCreate.response.ok) {
             setUserAuthorized(true);
-            const token = json.token;
+            const token = responseCreate.json.token;
             if (token) localStorage.setItem('token', JSON.stringify(token));
             navigate('/dashboard');
       }
@@ -36,18 +38,17 @@ const UserStorage = ({ children }) => {
   async function userLogin(data) {
     setLoading(true);
     try {
-      const response = await fetch(`${API_URL}login-prof`, {
+      const responseLogin = await request(`${API_URL}login-prof`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(data),
       });
-      const json = await response.json();
-      alert(json.message);
-      if (response.ok) {
+      alert(responseLogin.json.message);
+      if (responseLogin.response.ok) {
         setUserAuthorized(true);
-        const token = json.token;
+        const token = responseLogin.json.token;
         if (token) localStorage.setItem('token', JSON.stringify(token));
         navigate('/dashboard');
       }
@@ -58,9 +59,44 @@ const UserStorage = ({ children }) => {
       setLoading(false);
     }
   }
+  async function createALuno(data) {
+    try {
+        const responseAluno = await request(`${API_URL}aluno/create-aluno`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(data),
+        });
+        console.log(responseAluno)
+        if(responseAluno.response.ok) alert(responseAluno.json.message)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+   async function createModalidade(data) {
+    try{
+      const responseModalidade = await request(`${API_URL}modalidade/create-modalidade`, {
+       method: 'POST',
+       headers:{
+         'Content-Type' : 'application/json',
+         Authorization: `Bearer ${token}`
+       },
+       body: JSON.stringify(data)
+      });
+      if(responseModalidade.response.ok){
+         alert(responseModalidade.json.message)
+      }
+      
+    }catch(error){
+      console.log(error)
+    }
+
+   }
   async function verifyToken(token) {
     try {
-      const response = await fetch('http://localhost:3000/validate-token', {
+      const response = await fetch(`${API_URL}validate-token`, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
@@ -78,11 +114,9 @@ const UserStorage = ({ children }) => {
     }
   }
   useEffect(() => {
-    const tokenLocal = localStorage.getItem('token');
-    if (tokenLocal) {
-      const token = tokenLocal.slice(1, tokenLocal.length - 1);
+    if(token){
       verifyToken(token);
-    } else navigate('/login');
+    }else navigate('/login');
   }, []);
   return (
     <userContext.Provider
@@ -91,6 +125,8 @@ const UserStorage = ({ children }) => {
         loading,
         userCreate,
         userAuthorized,
+        createALuno,
+        createModalidade
       }}
     >
       {children}
