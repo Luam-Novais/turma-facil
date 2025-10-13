cd37c7567a18cccfc8a7972ee23cd5691e847537
Luam-Novais <luampabl014@gmail.com>
2025-10-13 13:10:19 -0300

    feat: implementar listagem de alunos com carregamento e exibição de modalidades

--- src/pages/authPages/ToListAlunos/ToListAlunos.jsx
+++ src/pages/authPages/ToListAlunos/ToListAlunos.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import styles from './ToListAlunos.module.scss';
+import Table from '../../../components/Table/Table';
+import useFetch from '../../../hooks/useFetch';
+import formaterToken from '../../../utils/formaterToken';
+import Loading from '../../../components/Loading/Loading';
+const API_URL = import.meta.env.VITE_API_URL;
 
 const ToListAlunos = () => {
-  return (
-    <div>ToListAlunos</div>
-  )
-}
+  const [alunos, setAlunos] = useState([]);
+  const { request, loading } = useFetch();
+  const token = formaterToken();
 
-export default ToListAlunos
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const responseAlunos = await request('http://localhost:3000/aluno/get-all-alunos', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        if(responseAlunos.response.ok) setAlunos(responseAlunos.json)
+      } catch (error) {console.log(error)}
+    }
+    fetchData();
+  }, []);
+
+  console.log(alunos)
+
+  if(loading){
+    return <Loading/>
+  }
+  if(alunos){
+    return (
+      <section className={styles.container}>
+        <h1>Todos Alunos</h1>
+        <Table>
+          <thead>
+            <tr>
+              <th>Indíce</th>
+              <th>Nome</th>
+              <th>Status</th>
+              <th>Modalidades</th>
+            </tr>
+          </thead>
+          <tbody>
+            {alunos.map((aluno, index) => {
+              return (
+                <tr className={`${aluno.status == 'inativo' ? styles.inactive : ''}`}>
+                  <td>{index + 1}</td>
+                  <td>{aluno.name}</td>
+                  <td>{aluno.status}</td>
+                  { aluno.modalidades instanceof Array &&
+                    <td>
+                      {aluno.modalidades.slice(0, 2).map((mod) => mod.name).join(', ')}
+                      {aluno.modalidades.length > 2 && `+${aluno.modalidades.length - 2}`}
+                    </td>
+                  }
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      </section>
+    );
+  }
+};
+
+export default ToListAlunos;
