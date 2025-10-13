c1386227be32aaa983799164bae91edd221ac2c3
Luam-Novais <luampabl014@gmail.com>
2025-10-13 13:04:12 -0300

    feat: adicionar componente Select com estilos

---
+++ src/components/Select/Select.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Select.module.scss'
+
+const Select = ({children, value, name, id, setValue}) => {
+  return (
+    <select name={name} id={id} value={value} onChange={({target}) => setValue(target.value)}>
+        <option value="">Selecione uma Modalidade</option>
+        {children}
+    </select>
+  )
+}
+
+export default Select
