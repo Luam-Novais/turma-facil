37152e2cf734c811d485eb954cb01ccd9f965027
Luam-Novais <luampabl014@gmail.com>
2025-10-13 19:57:58 -0300

    feat: adicionar estilo ao componente select utilizando a classe containerSelect

--- src/components/Select/Select.jsx
+++ src/components/Select/Select.jsx
@@ -3,7 +3,7 @@ import styles from './Select.module.scss'
 
 const Select = ({children, value, name, id, setValue}) => {
   return (
-    <select name={name} id={id} value={value} onChange={({target}) => setValue(target.value)}>
+    <select className={styles.containerSelect} name={name} id={id} value={value} onChange={({target}) => setValue(target.value)}>
         <option value="">Selecione uma Modalidade</option>
         {children}
     </select>
